ad80165e2333dbeb7a834470cdfb6d6f162640a8
andy <aksommerville@gmail.com>
2025-11-21 07:51:55 -0500

    wand-wielding tiles

--- src/game/sprite/sprite_hero.c
+++ src/game/sprite/sprite_hero.c
@@ -40,15 +40,20 @@ static void _hero_update(struct sprite *sprite,double elapsed) {
  
 static void _hero_render(struct sprite *sprite,int dstx,int dsty) {
   const int ht=NS_sys_tilesize>>1;
-  int col=0;
+  int bodyx=dstx+ht;
+  int bodyy=dsty+ht+NS_sys_tilesize;
+  uint8_t tileid=sprite->tileid;
+  uint8_t xform=sprite->xform;
+  #define ADDL(dx,dy,tid) graf_tile(&g.graf,bodyx+(xform?-(dx):(dx))*NS_sys_tilesize,bodyy+(dy)*NS_sys_tilesize,tid,xform);
   switch (SPRITE->animframe) {
-    case 0: col+=0; break;
-    case 1: col+=0; break;
-    case 2: col+=3; break;
-    case 3: col+=4; break;
+    case 0: tileid=0x10; break;
+    case 1: tileid=0x15; ADDL(1,0,0x16) break;
+    case 2: tileid=0x27; ADDL(0,-2,0x07) break;
+    case 3: tileid=0x18; ADDL(0,1,0x28) break;
   }
-  graf_tile(&g.graf,dstx+ht,dsty+ht,sprite->tileid+col-0x10,sprite->xform);
-  graf_tile(&g.graf,dstx+ht,dsty+ht+NS_sys_tilesize,sprite->tileid+col,sprite->xform);
+  graf_tile(&g.graf,bodyx,bodyy-NS_sys_tilesize,tileid-0x10,xform);
+  graf_tile(&g.graf,bodyx,bodyy,tileid,xform);
+  #undef ADDL
 }
 
 /* Type definition.
